f8cbb5a7f092c9915eb7dad8e5346e2fbb488bb1
Alexander Sharov <kvendingoldo@yandex.ru>
2023-09-16 15:55:35 +0200

    feat: prepare code for deploymnent

--- src/bot/bot.py
+++ src/bot/bot.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-import prettytable as pt
+
 import telebot
 import sys
 import logging
@@ -20,52 +20,111 @@ telebot.logger.setLevel(logging.INFO)
 text_messages = {
     'start': u'{name}, привет! 🎉 Выбери необходимое действие в меню ✨',
 
-    'help': '/help - показать данную подсказку',
+    'help': '/start - начальный экран \n/status - показать статус \n/help - показать подсказку',
+    'permission_error': 'Сорри, мы не знаем, кто ты. Напиши @kvendingoldo чтобы исправить доступ к боту.',
 
-    'wrong_msg': 'Похоже что-то пошло не так. Пожалуйста, воспользуйтесь подсказкой через /help или начните заного через /start'
+    'wrong_msg': 'Похоже что-то пошло не так. Пожалуйста, воспользуйся подсказкой через /help или начните заного через /start'
 }
-
-
-@bot.message_handler(commands=['help'])
-def handler_help(message):
-    try:
-        bot.send_message(
-            message.from_user.id,
-            text_messages['help'],
-            reply_markup=ReplyKeyboardRemove()
-        )
-    except Exception as ex:
-        logging.error(ex)
 
 
 @bot.message_handler(commands=['start'])
 def handler_start(message):
-    markup = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True)
-    markup.row('выговориться', 'посмотреть историю', 'помощь')
+    if message.from_user.username not in config["allowed_users"]:
+        try:
+            msg = bot.send_message(
+                message.from_user.id,
+                text_messages['permission_error'].format(name=message.from_user.first_name)
+            )
+        except Exception as ex:
+            logging.error(ex)
+    else:
+        markup = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True)
+        markup.row('показать статус', 'помощь')
 
-    try:
-        msg = bot.send_message(
-            message.from_user.id,
-            text_messages['start'].format(name=message.from_user.first_name),
-            reply_markup=markup
-        )
-    except Exception as ex:
-        logging.error(ex)
+        try:
+            msg = bot.send_message(
+                message.from_user.id,
+                text_messages['start'].format(name=message.from_user.first_name),
+                reply_markup=markup
+            )
+        except Exception as ex:
+            logging.error(ex)
 
 
 @bot.message_handler(commands=['status'])
-def handler_start(message):
-    markup = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True)
+def handler_status(message):
+    if message.from_user.username not in config["allowed_users"]:
+        try:
+            msg = bot.send_message(
+                message.from_user.id,
+                text_messages['permission_error'].format(name=message.from_user.first_name)
+            )
+        except Exception as ex:
+            logging.error(ex)
+    else:
+        try:
+            table = check.check_status(config["clients"])
+            msg = bot.send_message(
+                message.from_user.id,
+                f"```{table}```",
+                parse_mode='MarkdownV2'
+            )
+        except Exception as ex:
+            logging.error(ex)
 
-    try:
-        table = check.check_status(config["clients"])
-        msg = bot.send_message(
-            message.from_user.id,
-            f"```{table}```",
-            parse_mode='MarkdownV2'
-        )
-    except Exception as ex:
-        logging.error(ex)
+
+@bot.message_handler(commands=['help'])
+def handler_help(message):
+    if message.from_user.username not in config["allowed_users"]:
+        try:
+            msg = bot.send_message(
+                message.from_user.id,
+                text_messages['permission_error'].format(name=message.from_user.first_name)
+            )
+        except Exception as ex:
+            logging.error(ex)
+    else:
+        try:
+            bot.send_message(
+                message.from_user.id,
+                text_messages['help'],
+                reply_markup=ReplyKeyboardRemove()
+            )
+        except Exception as ex:
+            logging.error(ex)
+
+
+@bot.message_handler(content_types=['text'])
+def handler_text(message):
+    if message.from_user.username not in config["allowed_users"]:
+        try:
+            msg = bot.send_message(
+                message.from_user.id,
+                text_messages['permission_error'].format(name=message.from_user.first_name)
+            )
+        except Exception as ex:
+            logging.error(ex)
+    else:
+        if message.text == 'показать статус':
+            try:
+                table = check.check_status(config["clients"])
+                msg = bot.send_message(
+                    message.from_user.id,
+                    f"```{table}```",
+                    parse_mode='MarkdownV2'
+                )
+            except Exception as ex:
+                logging.error(ex)
+
+        elif message.text == 'помощь':
+            try:
+                bot.send_message(
+                    message.from_user.id,
+                    text_messages['help'],
+                    reply_markup=ReplyKeyboardRemove()
+                )
+            except Exception as ex:
+                logging.error(ex)
 
 
 def run():
--- src/mks/check.py
+++ src/mks/check.py
@@ -1,41 +1,28 @@
 # -*- coding: utf-8 -*-
+import logging
 
 import prettytable as pt
 import requests
-import json
-
-
-def fetch_json(url):
-    try:
-        response = requests.get(url)
-        response.raise_for_status()  # Raise an exception for HTTP errors
-        json_data = response.json()  # Parse JSON response
-        return json_data
-    except requests.exceptions.RequestException as e:
-        print(f"Error fetching data from {url}: {e}")
-        return None
-    except json.JSONDecodeError as e:
-        print(f"Error parsing JSON from {url}: {e}")
-        return None
 
 
 def check_status(clients):
     table = pt.PrettyTable(['клиент', 'версия', 'статус', 'облако'])
 
-
-
     for client in clients:
-        response = requests.get(f"{client['url']}/{client['endpoint']}")
-        response.raise_for_status()  # Raise an exception for HTTP errors
-        plain_text = response.text  # Get plain text response
+        try:
+            response = requests.get(f"{client['url']}/{client['endpoint']}")
+            response.raise_for_status()
+            version = response.text
+            status = "✅"
+        except Exception as ex:
+            logging.error(ex)
+            version = "❓"
+            status = "❌"
 
-        if plain_text:
-            table.add_row([
-                client['name'], plain_text, "✅", client['type']
-            ])
-
+        table.add_row([
+            client['name'], version, status, client['type']
+        ])
 
     print(table)
-
 
     return table
