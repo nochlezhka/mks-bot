1db5e375194f749675b9f4ad03d0de26d2114c84
Alexander Sharov <kvendingoldo@yandex.ru>
2023-09-16 16:14:36 +0200

    fix: configuration

--- src/mks/check.py
+++ src/mks/check.py
@@ -6,24 +6,40 @@ import requests
 
 
 def check_status(clients):
-    table = pt.PrettyTable(['клиент', 'версия', 'статус', 'облако'])
+    table = pt.PrettyTable(['клиент', 'версия', 'статус', 'TLS', 'облако'])
 
     for client in clients:
         try:
             response = requests.get(
-                url=f"{client['url']}/{client['endpoint']}",
+                url=f"{client['url']}/",
                 timeout=60
             )
             response.raise_for_status()
-            version = response.text
             status = "✅"
+            ssl_status = "✅"
+        except requests.exceptions.SSLError as ex:
+            logging.error(f"SSL certificate error: {ex}")
+            status = "❌"
+            ssl_status = "❌"
+        except Exception as ex:
+            logging.error(ex)
+            status = "❌"
+            ssl_status = "❌"
+
+        try:
+            response = requests.get(
+                url=f"{client['url']}/{client['endpoint']}",
+                timeout=60,
+                verify=False
+            )
+            response.raise_for_status()
+            version = response.text
         except Exception as ex:
             logging.error(ex)
             version = "❓"
-            status = "❌"
 
         table.add_row([
-            client['name'], version, status, client['type']
+            client['name'], version, status, ssl_status, client['type']
         ])
 
     print(table)
