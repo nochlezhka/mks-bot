b8972d531aa5d959542bf3b7fa6c2c56dbcbfaaf
Alexander Sharov <kvendingoldo@yandex.ru>
2025-02-09 19:50:16 +0400

    feat: improve initial implementation

--- src/daemons/alerts.py
+++ src/daemons/alerts.py
@@ -5,32 +5,80 @@ import telebot
 
 import logging
 
+from mks import check
+
+text_messages = {
+    'failed': u'[FAILED] {name} check for {client_name}',
+    'resolved': u'[RESOLVED] {type} check for {client_name}'
+}
+
+alerts_inmem_db = {}
+
+
+def send_alert(config, client):
+    bot = telebot.TeleBot(config["telegram"]["token"])
+
+    for alert_name in alerts_inmem_db[client]:
+        if alerts_inmem_db[client][alert_name]["failed"] > 3:
+            for receiver in config["alerts"]["receivers"]:
+                bot.send_message(
+                    receiver, text_messages['failed'].format(
+                        name=alert_name,
+                        client_name=client
+                    )
+                )
+        if alerts_inmem_db[client][alert_name]["resolved"] > 3:
+            for receiver in config["alerts"]["receivers"]:
+                bot.send_message(
+                    receiver, text_messages['resolved'].format(
+                        name=alert_name,
+                        client_name=client
+                    )
+                )
+            alerts_inmem_db[client][alert_name]["resolved"] = 0
+            alerts_inmem_db[client][alert_name]["failed"] = 0
+
+
+def update_inmem_db(type, state):
+    if state != "ok":
+        alerts_inmem_db[type]['status']['failed'] += 1
+    else:
+        if alerts_inmem_db[type]['status']['failed'] != 0:
+            alerts_inmem_db[type]['status']['resolved'] += 1
+        else:
+            alerts_inmem_db[type]['status']['resolved'] = 0
+
 
 def run(config):
     logging.info("Hello from MKS alerts system")
-
-    bot = telebot.TeleBot(config["telegram"]["token"])
-    telebot.logger.setLevel(logging.INFO)
+    alert_types = ["healthy", "ssl_status"]
 
     while True:
         header, items = check.get_long_status(config["clients"], config["default_version_endpoint"])
 
         for item in items:
-            client_name, mks_version, mks_status, mks_ssl_status, client_type, client_url = item
+            client_info = {
+                "name": item[0],
+                "healthy": item[2],
+                "ssl_status": item[3]
+            }
 
-            if mks_status != "ok":
-                bot.send_message(
-                    usr1.id, 'FAIL!'
-                )
-            else:
-                if mks_ssl_status != "ok":
-                    bot.send_message(
-                        usr1.id, 'FAIL!'
-                    )
+            if client_info["name"] not in alerts_inmem_db:
+                alerts_inmem_db[client_info["name"]] = {
+                    'healthy': {
+                        "failed": 0,
+                        "resolved": 0
+                    },
+                    'ssl_status': {
+                        "failed": 0,
+                        "resolved": 0
+                    }
+                }
 
-                if mks_version == "?":
-                    bot.send_message(
-                        usr1.id, 'FAIL!'
-                    )
+            for alert_type in alert_types:
+                update_inmem_db(alert_type, client_info[alert_type])
+
+        for client in alerts_inmem_db:
+            send_alert(config, client)
 
         time.sleep(config["alerts"]["check_period"])
